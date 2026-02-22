220bbebcba7d25f7b8719d7849030520222a4947
StevenWill <ste.d.williams@gmail.com>
2026-02-22 14:56:18 -0600

    convert knightfall to monorepo

--- apps/nightfall/src/app/app.component.spec.ts
+++ apps/nightfall/src/app/app.component.spec.ts
@@ -22,10 +22,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('nightfall');
   });
 
-  it('should render title', () => {
+  it('should render title in header', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('nightfall app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Nightfall');
   });
 });
